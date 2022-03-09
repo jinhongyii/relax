43c32bc247ec404d80ebd05482bc1189f2ea439d
Siyuan Feng <hzfengsy@sjtu.edu.cn>
2022-03-09 08:04:15 +0000

    graph partitioner

---
+++ src/relax/transform/fuse_ops.cc
@@ -0,0 +1,345 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*!
+ * \file src/relax/transform/fma_rewrite.cc
+ * \brief Perform fused multiply add rewriting in dataflow blocks.
+ */
+
+#include <tvm/relax/expr_functor.h>
+#include <tvm/relax/transform.h>
+
+#include "../../relay/analysis/graph_partitioner.h"
+#include "../../support/arena.h"
+namespace tvm {
+namespace relax {
+
+/*
+  Note on Fusing algorithm:
+
+  The main challenge of general fusor is to handle possible diamond shape branches,
+  in the following graph, conv2d can be fused to elemwise add.
+
+            conv2d
+            /  |  \
+           /   |   \
+         op    op   op
+          \    |    /
+           \   |   /
+          elemwise add
+               |
+
+  However, at the point of conv2d we do not necessarily know that all the future paths
+  will merge at the elemwise add. The fusion algorithm applies post-dominator analysis.
+
+  The immediate post-dominator of a node defined by the closest node where all the future path goes
+  into. In the above case, the elemwise add is the post-dominator of conv2d. The general algorithm
+  is as follows:
+
+  - Construct a DAG of dataflow graph for dominator analysis
+  - Construct a post-dominator tree which gives immediate post dominator of each node.
+  - Run fusion algorithm with the given post-dominator information.
+
+  Note that, because we run analysis on a DAG, we use a single pass post-dominator
+  tree construction algorithm via LCA, which is simpler than the full version that handles cycles.
+
+  The fusion algorithm traverses from each node and checks if it can be fused to its
+  immediate post dominator. It has to check the following things:
+
+  - CheckPath: check all the path between a node and its immediate post-dominator
+               satisfies the fuse condition.
+  - Note that these intermediate node can already be fused with another nodes, the algorithm
+      will still run correctly.
+  - CommitFuse: mark all the nodes between source and post-dominator as the same group.
+  - We use an Union-Find data structure to manage the groups.
+*/
+
+using relay::GraphPartitioner;
+using relay::IndexedForwardGraph;
+using relay::OpPatternKind;
+using support::LinkedList;
+using support::LinkNode;
+
+constexpr uint32_t kMaxFusedOps = 256;
+
+TVM_REGISTER_PASS_CONFIG_OPTION("relax.FuseOps.max_depth", Integer);
+
+// Creator of post dominator tree of the dataflow
+class IndexedForwardGraphCreator : private ExprVisitor {
+ public:
+  static IndexedForwardGraph Create(support::Arena* arena, const IRModule& mod) {
+    GlobalVar main_global_var = mod->GetGlobalVar("main");
+    Function body = Downcast<Function>(mod->Lookup(main_global_var));
+    IndexedForwardGraphCreator creator(arena, mod);
+    creator.VisitExpr(body);
+    // creator.graph_.DebugDump();
+    return creator.graph_;
+  }
+
+ private:
+  void VisitExpr_(const ConstantNode* op) final {
+    this->AddNode(op);
+    IndexedForwardGraph::Node* node = graph_.node_map.at(op);
+    DataType dtype = DataType(op->data->dtype);
+    // This rule must be consistent with code generator.
+    bool is_simple_const =
+        (dtype == DataType::Int(32) || dtype == DataType::Int(64) || dtype == DataType::Float(32) ||
+         dtype == DataType::Float(64) || dtype == DataType::Bool());
+    if (op->is_scalar() && is_simple_const) {
+      node->pattern = OpPatternKind::kElemWise;
+    } else {
+      // for now, mark non-scalar constant
+      // as opaque, we will not choose to fuse it.
+      node->pattern = OpPatternKind::kOpaque;
+    }
+  }
+
+  void VisitExpr_(const FunctionNode* op) final {
+    for (const Var& param : op->params) {
+      this->vmap_[param.get()] = param;
+      CreateNode(param.get());
+      this->UpdateEdge(param, nullptr, OpPatternKind::kOpaque);
+    }
+    ExprVisitor::VisitExpr_(op);
+  }
+
+  void VisitExpr_(const VarNode* op) final { this->AddNode(op); }
+
+  void VisitExpr_(const SeqExprNode* op) {
+    for (BindingBlock block : op->blocks) {
+      this->VisitBindingBlock(block);
+    }
+    // Don't need to visit return value
+    // this->VisitExpr(op->body);
+  }
+
+  void VisitBindingBlock_(const BindingBlockNode* block) final {
+    // Skip Binding Block since it's imprue (with side effect or control flow)
+    return;
+  }
+
+  void VisitBinding_(const VarBindingNode* binding) final {
+    // Don't allow recursive var binding.
+    ICHECK(!cur_binding_var_.defined());
+    cur_binding_var_ = binding->var;
+    ICHECK(!binding->value->IsInstance<VarNode>());
+    this->vmap_[binding->var.get()] = binding->value;
+    CreateNode(binding->value.get());
+    if (!binding->var->IsInstance<DataflowVarNode>()) {
+      this->UpdateEdge(binding->value, nullptr, OpPatternKind::kOpaque);
+    }
+    ExprVisitor::VisitBinding_(binding);
+    cur_binding_var_ = NullOpt;
+  }
+
+  void VisitExpr_(const CallNode* op) final {
+    static const Op& call_tir_op_ = Op::Get("relax.call_tir");
+
+    ICHECK(cur_binding_var_.defined());
+    const Var& binding_var = cur_binding_var_.value();
+    auto it = graph_.node_map.find(binding_var.get());
+    ICHECK(it != graph_.node_map.end());
+    IndexedForwardGraph::Node* node = it->second;
+
+    // If the pattern is not annotated we will default to opaque.
+    OpPatternKind op_pattern = OpPatternKind::kOpaque;
+    ICHECK(op->op->IsInstance<OpNode>());
+    if (op->op == call_tir_op_) {
+      const Expr& shape = op->args[0];
+      GlobalVar global_var = Downcast<GlobalVar>(op->args[1]);
+      tir::PrimFunc func = Downcast<tir::PrimFunc>(orginal_mod_->Lookup(global_var));
+      const Tuple& args = Downcast<Tuple>(op->args[2]);
+      int func_pattern = func->GetAttr<Integer>("op_pattern").value_or(OpPatternKind::kOpaque);
+
+      // TODO(siyuan): Check the integer data is valid
+      op_pattern = static_cast<OpPatternKind>(func_pattern);
+
+      for (const Expr& arg : args->fields) {
+        this->UpdateEdge(arg, node, op_pattern);
+        this->VisitExpr(arg);
+      }
+    } else {
+      LOG(FATAL) << "The call op " << op->op << " is not supported in dataflow block for now.";
+    }
+    node->pattern = op_pattern;
+    this->AddNode(binding_var.get());
+  }
+
+  void VisitExpr_(const TupleGetItemNode* op) final {
+    ICHECK(cur_binding_var_.defined());
+    const Var& binding_var = cur_binding_var_.value();
+    auto it = graph_.node_map.find(op);
+    ICHECK(it != graph_.node_map.end());
+    IndexedForwardGraph::Node* node = it->second;
+
+    node->pattern = OpPatternKind::kInjective;
+    this->UpdateEdge(op->tuple, node, OpPatternKind::kInjective);
+    this->AddNode(binding_var.get());
+  }
+
+  void VisitExpr_(const IfNode* op) final {
+    LOG(INFO) << "Dataflow block expects no Control flow inside.";
+  }
+
+ private:
+  explicit IndexedForwardGraphCreator(support::Arena* arena, const IRModule& mod)
+      : orginal_mod_(mod), arena_(arena) {}
+
+  // Helper functions to maintain IndexedForwardGraph
+  /*!
+   * \brief The
+   * \param node The Relax IR nodes
+   * \param parent The parent node in the IndexedForwardGraph.
+   *               The source is external if the parent is nullptr.
+   * \param pattern The relation pattern between the node and its parent.
+   */
+  void UpdateEdge(Expr node, IndexedForwardGraph::Node* parent, OpPatternKind pattern) {
+    if (const VarNode* var = node.as<VarNode>()) {
+      auto it = vmap_.find(var);
+      ICHECK(it != vmap_.end());
+      node = it->second;
+    }
+    const tvm::Object* key = node.get();
+    auto it = graph_.node_map.find(key);
+    ICHECK(it != graph_.node_map.end());
+    IndexedForwardGraph::Node* current = it->second;
+    if (parent != nullptr) {
+      auto* link = arena_->make<LinkNode<IndexedForwardGraph::Edge>>();
+      link->value.node = parent;
+      link->value.pattern = pattern;
+      current->outputs.Push(link);
+    } else {
+      current->extern_ref = true;
+    }
+  }
+
+  void AddNode(const tvm::Object* key) {
+    auto it = graph_.node_map.find(key);
+    ICHECK(it != graph_.node_map.end()) << "Cannot find node " << GetRef<ObjectRef>(key);
+    IndexedForwardGraph::Node* node = it->second;
+    if (node->ref == nullptr) {
+      node->ref = key;
+      node->index = graph_.post_dfs_order.size();
+      graph_.post_dfs_order.push_back(node);
+    } else {
+      ICHECK(node->ref == key);
+    }
+  }
+
+  IndexedForwardGraph::Node* CreateNode(const tvm::Object* key) {
+    ICHECK(graph_.node_map.find(key) == graph_.node_map.end());
+    IndexedForwardGraph::Node* node = arena_->make<IndexedForwardGraph::Node>();
+    graph_.node_map[key] = node;
+    return node;
+  }
+
+ private:
+  /*! \brief The whole IRModule */
+  const IRModule& orginal_mod_;
+  /*! \brief Allocator of all the internal node object */
+  support::Arena* arena_;
+  /*! \brief The output graph */
+  IndexedForwardGraph graph_;
+  /*! \brief Attribute equal comparator */
+  StructuralEqual attr_equal_;
+  /*! \brief Var binding map */
+  std::unordered_map<const VarNode*, Expr> vmap_;
+  /*! \brief current binding var */
+  Optional<Var> cur_binding_var_ = NullOpt;
+};
+
+class FuseMutator : public ExprMutator {
+ public:
+  // Run the transform
+  static IRModule Transform(const IRModule& mod, int fuse_opt_level, size_t max_fuse_depth) {
+    // setup the group map.
+    FuseMutator fuse_mutator(mod);
+    GlobalVar main_global_var = mod->GetGlobalVar("main");
+    Function main_func = Downcast<Function>(mod->Lookup(main_global_var));
+    auto graph = IndexedForwardGraphCreator::Create(&fuse_mutator.arena_, mod);
+    auto groups =
+        GraphPartitioner(&fuse_mutator.arena_, fuse_opt_level, max_fuse_depth).Partition(graph);
+    for (size_t nid = 0; nid < graph.post_dfs_order.size(); ++nid) {
+      ICHECK(graph.post_dfs_order[nid]->ref != nullptr);
+      fuse_mutator.gmap_[graph.post_dfs_order[nid]->ref] = groups[nid];
+      fuse_mutator.ginfo_[groups[nid]] = GroupInfo();
+    }
+    // The following line can be used for debug.
+    fuse_mutator.DebugDumpGroup(main_func);
+
+    return mod;
+  }
+
+ private:
+  explicit FuseMutator(const IRModule& mod) : orginal_mod_(mod) {}
+
+ private:
+  // Debug function, dump the group assignment in text.
+  void DebugDumpGroup(const Expr& body) {
+    std::unordered_map<const GraphPartitioner::Group*, size_t> group_id;
+    auto get_id = [&group_id](const GraphPartitioner::Group* group) -> size_t {
+      auto it = group_id.find(group);
+      if (it == group_id.end()) {
+        return group_id[group] = group_id.size();
+      } else {
+        return it->second;
+      }
+    };
+
+    auto print_object = [](const ObjectRef& object) -> std::string {
+      std::ostringstream os;
+      if (const auto* var = object.as<VarNode>()) {
+        os << "Var(" << var->name_hint() << ")";
+      } else if (const auto* call = object.as<CallNode>()) {
+        ICHECK(call->op == Op::Get("relax.call_tir"));
+        os << call->args[1];
+      } else {
+        os << object;
+      }
+      return os.str();
+    };
+
+    for (const auto& kv : gmap_) {
+      const ObjectRef& object = GetRef<ObjectRef>(kv.first);
+      const GraphPartitioner::Group* group = kv.second->FindRoot();
+      LOG(INFO) << print_object(object) << ": Group #" << get_id(group);
+    }
+  }
+};
+
+namespace transform {
+
+Pass FuseOps(int fuse_opt_level) {
+  runtime::TypedPackedFunc<IRModule(IRModule, PassContext)> pass_func =  //
+      [=](IRModule m, PassContext pc) {
+        int opt_level = fuse_opt_level == -1 ? pc->opt_level : fuse_opt_level;
+        auto max_fuse_depth = pc->GetConfig("relax.FuseOps.max_depth", Integer(kMaxFusedOps));
+        return FuseMutator::Transform(m, opt_level, max_fuse_depth.value());
+      };
+  return CreateModulePass(/*pass_function=*/pass_func,  //
+                          /*opt_level=*/0,              //
+                          /*pass_name=*/"FuseOps",      //
+                          /*required=*/{});
+}
+
+TVM_REGISTER_GLOBAL("relax.transform.FuseOps").set_body_typed(FuseOps);
+
+}  // namespace transform
+
+}  // namespace relax
+}  // namespace tvm
---
+++ tests/python/relax/test_transform_fuse_ops.py
@@ -0,0 +1,75 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from __future__ import annotations  # must import to defer parsing of annotations
+import pytest
+
+from tvm import relax
+
+import tvm.script
+from tvm.script import tir as T, relax as R
+
+from tvm.relax.testing import resnet
+
+
+def test_fuse_simple():
+    """Simple testcase."""
+
+    @tvm.script.ir_module
+    class before:
+        @R.function
+        def main(x: Tensor[(m, n), "float32"]):
+            with R.dataflow():
+                lv0: Tensor[(m, n), "float32"] = R.call_tir((m, n), f1, (x, x))
+                gv0: Tensor[(m, n), "float32"] = R.call_tir((m, n), f2, (lv0, x))
+                R.output(gv0)
+            return gv0
+
+        @T.prim_func
+        def f1(a: T.handle, b: T.handle, c: T.handle):
+            T.func_attr({"global_symbol": "f1", "tir.noalias": True, "op_pattern": 0})
+            m = T.var("int32")
+            n = T.var("int32")
+            A = T.match_buffer(a, (m, n))
+            B = T.match_buffer(b, (m, n))
+            C = T.match_buffer(c, (m, n))
+            with T.block():
+                T.evaluate(1)
+
+        @T.prim_func
+        def f2(a: T.handle, b: T.handle, c: T.handle):
+            T.func_attr({"global_symbol": "f2", "tir.noalias": True, "op_pattern": 0})
+            m = T.var("int32")
+            n = T.var("int32")
+            A = T.match_buffer(a, (m, n))
+            B = T.match_buffer(b, (m, n))
+            C = T.match_buffer(c, (m, n))
+            with T.block():
+                T.evaluate(2)
+
+    mod = relax.transform.FuseOps()(before)
+    print(R.parser.astext(mod))
+
+
+def test_fuse_resnet():
+    mod = resnet.ResNet50
+    mod = relax.transform.AnnotateOpKind()(mod)
+    mod = relax.transform.FuseOps()(mod)
+
+
+if __name__ == "__main__":
+    # test_fuse_simple()
+    test_fuse_resnet()
